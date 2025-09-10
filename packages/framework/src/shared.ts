90a7cbcb1da232a460140188f6c121110f099cc9
Bassel Koshak <bassel.koshak@ejari.sa>
2025-09-10 16:46:38 +0300

    refactor: rename maqsam chat config for consistency

--- packages/framework/src/shared.ts
+++ packages/framework/src/shared.ts
@@ -172,7 +172,7 @@ export enum ChatProviderIdEnum {
   RocketChat = 'rocket-chat',
   WhatsAppBusiness = 'whatsapp-business',
   ChatWebhook = 'chat-webhook',
-  Maqsam = 'maqsam',
+  MaqsamWhatsApp = 'maqsam-whatsapp',
 }
 
 export enum PushProviderIdEnum {
